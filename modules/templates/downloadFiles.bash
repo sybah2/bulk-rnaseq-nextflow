7c4437af8d1934bde235ba961e0c3d4b52903627
sybah2 <saikouybah@gmail.com>
2023-06-02 16:00:47 +0100

    Fix sra download

--- modules/templates/downloadFiles.bash
+++ modules/templates/downloadFiles.bash
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-fasterq-dump --split-3 ${id}
+
+if [ -s $HOME/.ncbi/user-settings.mkfg ]
+then
+    fasterq-dump --split-3 ${id}
+
+else
+    mkdir -p $HOME/.ncbi
+    cp /usr/bin/user-settings.mkfg $HOME/.ncbi/
+    fasterq-dump --split-3 ${id}
+fi
